a91400d046bef4bb4ae4e8e9f5a46fcd59fae4ed
Pablito-CG <girardc101@outlook.com>
2024-12-10 20:47:53 -0500

    Initializing repo

---
+++ html/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Finance </title>
+</head>
+<body>
+	<main>
+		<h1>hello there</h1>
+	</main>
+	<?php
+		echo "hello again";
+		$servername = "localhost";
+		$username = "root";
+		$password = "#1FrogLegs?"
+		$db_name = "finance";
+		
+		$conn = new mysqli($servername,$username,$password,$db_name);
+		if($conn->connect_error) {
+			echo "Connection failed: " . $conn->connect_error;
+		}
+	?>
+</body>
+</html>
