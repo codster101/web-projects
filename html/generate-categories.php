13201b85d19bfcf94712c2744f031621e8ad4075
Pablito-CG <girardc101@outlook.com>
2025-01-14 22:28:30 -0500

    Drop down for category

--- html/generate-categories.php
+++ html/generate-categories.php
@@ -1,6 +1,53 @@
 <?php
 
-	echo "<option value='test'>Test</option>";
+	$categories = array(
+		"Auto Insurance"	=> "Auto Insurance",
+		"Auto License & Taxes"	=> "Auto License & Taxes",
+		"Bars"			=> "Bars",
+		"Books"			=> "Books",
+		"Car Payment"		=> "Car Payment",
+		"Car Repairs"		=> "Car Repairs",
+		"Cleaning/Laundary"	=> "Cleaning/Laundry",
+		"Clothes"		=> "Clothes",
+		"Cosemetics/Hair Care"	=> "Cosemetics/Hair Care",
+		"Dentist"		=> "Dentist",
+		"Doctors"		=> "Doctors",
+		"Electricity"		=> "Electricity",
+		"Entertainment"		=> "Electricity",
+		"Gas Bill"		=> "Gas Bill",
+		"Gas & Oil"		=> "Gas & Oil",
+		"Gifts"			=> "Gifts",
+		"Groceries"		=> "Groceries",
+		"Gym"			=> "Gym",
+		"Health Insurance"	=> "Health Insurance",
+		"Hobbies"		=> "Hobbies",
+		"Home Furnishings"	=> "Home Furnishings",
+		"Home Repairs/Maint"	=> "Home Repairs/Maint",
+		"Internet/Cable"	=> "Internet/Cable",
+		"Life Insurance"	=> "Life Insurance",
+		"Liquor"		=> "Liquor",
+		"Medication"		=> "Medication",
+		"Office Supplies"	=> "Office Supplies",
+		"Optometrist"		=> "Optometrist",
+		"Pet Supplies"		=> "Pet Supplies",
+		"Phone/Mobile"		=> "Phone/Mobile",
+		"Piano Lessons"		=> "Piano Lessons",
+		"Rent"			=> "Rent",
+		"Restaurants"		=> "Restaurants",
+		"Savings Account"	=> "Savings Account",
+		"Student Loans (Required)"		=> "Student Loans (Required)",
+		"Student Loans (Extra)"	=> "Student Loans (Extra)",
+		"Tithes"		=> "Tithes",
+		"Trash"			=> "Trash",
+		"Uber/Parking"		=> "Uber/Parking",
+		"Vacation"		=> "Vacation",
+		"Vitamins"		=> "Vitamins",
+		"Water"			=> "Water",
+	);
+	
+	foreach($categories as $k => $v) {
+		echo "<option value=$k>$v</option>";
+	}
 
 
 
