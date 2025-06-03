5bf8794a512bf11e5356524c828f52285fd7f35d
Pablito-CG <girardc101@outlook.com>
2025-06-03 06:28:40 -0400

    Adding clock

--- html/game_of_life/clock.js
+++ html/game_of_life/clock.js
@@ -1,24 +1,18 @@
-export function Clock(g) {
-	let clock_on = true;
-	let clock_cycle = 1	// Length of a cycle in seconds
-	let last_cycle_time = Date.now();
+export function Clock(g, t) {
+	let clock_cycle = t;	// Length of a cycle in seconds
+	let interval_id = 0;
 
 	function run() {
-		while (clock_on) {
-			// Run cycle events if sufficient time has passed since the last cycle
-			let diff = (last_cycle_time - Date.now()) / 1000;
-			if (diff > clock_cycle) {
-				cycle_events();
-				last_cycle_time = Date.now();
-			}
-		}
+		interval_id = setInterval(cycle_events, clock_cycle);
 	}
 
 	function cycle_events() {
 		g.manage_population();
 	}
 
-	function stop() { clock_on = false; }
+	function stop() {
+		clearInterval(interval_id);
+	}
 
 	return {
 		run,
